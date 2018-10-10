a19100ddd640b8799aef707774960ffa8971a095
John Wanja Muiru <geesgoos@gmail.com>
2018-10-10 14:21:04 +0300

    Kulanasi first version design.

--- app.js
+++ app.js
@@ -1,89 +1,21 @@
-const fortune = require('./lib/fortune.js');
+const express = require('express');
+const app = express();
 
- const express = require('express');
+app.set('port', process.env.PORT || 3000);
 
- 
- 	const app = express();
-
- 	
- 	const handlebars = 
-require('express-handlebars').create({defaultLayout:'main'});
-
-	app.use(express.static(__dirname + '/public'));
-
- 	app.engine('handlebars', handlebars.engine);
-
- 	app.set('view engine', 'handlebars');
-
- 	
- 	app.set('port', process.env.PORT || 3000);
-
- 	
- 	app.get('/', (req,res)=>{
-
- 		//res.type('text/plain');
-
- 		//res.send('Meadwlark Travel');
-
-		res.render('home');
-
- 	});
-
- 	
- 	app.get('/about',(req, res)=>{
-
- 		//res.type('text/plain');
-
- 		//res.send('About Meadowlark Travel');
-
-		/*var randomFortune = 
-fortunes[Math.floor(Math.random()*fortunes.length)];*/
-
-		res.render('about',{fortune:fortune.getFortune()})
-
+//lets handle custom error
+app.use((req,res)=>{
+	res.type('text/plain');
+	res.status(404);
+	res.send('404 - Not Found');
+});
+app.use((err, req, res, next)=>{
+	console.log.error(err.stack);
+	res.type('text/plain');
+	res.status(500);
+	res.send('500 - Server Error');
 });
 
-
-	/*var fortunes = [
-
-		"Conquer your feers or they will conquer you.",
-
-		"Rivers need springs.",
-
-		"Do not fear what you don't know.",
-
-		"You will have a pleasant surprise.",
-
-		"Whenever possible, keep it simple.",
-
-			];*/
-
- 	
- 	//custom 404 page
-
- 	app.use((req,res)=>{
-
- 		res.type('text/plain');
-
- 		res.status(404);
-
- 		res.send('404 - Not Found');
-
- 	});
-
- 	
- 	//custom 500 page
- 	app.use((err, req,res,next)=>{
- 		console.error(err.stack);
- 		res.type("text/plain");
- 		res.status(500);
- 		res.send('500 - Server Error');
-
- 	});
- 	
-
- 	//create and listen
- 	
- 	app.listen(app.get('port'), ()=>{
- 		console.log('Express started on http://localhost:'+app.get('port')+';press Ctrl-C to terminate.');
- 	}); 
+app.listen(app.get('port'), ()=>{
+	console.log('Express started at'+app.get('port'));
+});
